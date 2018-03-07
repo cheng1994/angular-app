3782bd25328bcc23283c1c7f17846086211731b4
Gordon Chen <gordon.chen94@gmail.com>
2018-03-06 19:40:33 -0800

    firebase ci configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+const config = {
+    apiKey: "AIzaSyBNHTZztQh5fXjbvA0P1aCcD0dGc5z7-es",
+    authDomain: "gordon-392f2.firebaseapp.com",
+    databaseURL: "https://gordon-392f2.firebaseio.com",
+    projectId: "gordon-392f2",
+    storageBucket: "gordon-392f2.appspot.com",
+    messagingSenderId: "935091986574"
+};
+
 
 @NgModule({
   declarations: [
